be7c1a31867af12b1b5cd116a85507a84efbb93c
NIRMIT SHAH <93076911+nirmit1@users.noreply.github.com>
2023-10-16 11:50:44 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -14,4 +14,5 @@ if st.button('Classify'):
     prediction = model.predict([user_input])[0]
     st.write('Prediction:', 'Fake' if prediction == 0 else 'Real')
 
-streamlit run fake_news_classification (2).py
+# streamlit run fake_news_classification (2).py
+streamlit run fake_news_classification (2).py --server.port 8502
