4ab10873a1af03f5a5ba379cf85555caa9a8fc9d
NIRMIT SHAH <93076911+nirmit1@users.noreply.github.com>
2023-10-16 11:41:32 +0530

    Update fake_news_classification (2).py

--- fake_news_classification (2).py
+++ fake_news_classification (2).py
@@ -289,3 +289,18 @@ Prediction(user_data,rf).prediction_model()
 user_data = 'Massachusetts Cops Wife Busted for Pinning Fake Home-Invasion Robbery on Black Lives Matter'
 Prediction(user_data,rf).prediction_model()
 
+!pip install streamlit
+import streamlit as st
+import joblib
+
+# Load your machine learning model
+model = joblib.load('your_model.joblib')
+
+# Define the Streamlit app
+st.title('News Classification App')
+
+user_input = st.text_area('Enter a news article:')
+if st.button('Classify'):
+    prediction = model.predict([user_input])[0]
+    st.write('Prediction:', 'Fake' if prediction == 0 else 'Real')
+streamlit run your_app.py
